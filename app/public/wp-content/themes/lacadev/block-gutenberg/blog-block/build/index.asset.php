232e7170e96254b9d2119fe34d93d1cbb299e1b5
aiot-anhd1 <anhd1@aiot-global.com>
2026-03-11 15:49:01 +0700

    update gutenberg blocks

---
+++ app/public/wp-content/themes/lacadev/block-gutenberg/blog-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '7b062148aecfcea7eb87');
---
+++ app/public/wp-content/themes/lacadev/block-gutenberg/service-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '905e1c5f07017f34189e');
