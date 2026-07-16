425e0c0bee54d72be5b0bd2c1d4f85454c1df848
lacadev <mooms.dev@gmail.com>
2026-07-17 01:31:14 +0700

    add portfolio template

---
+++ app/public/wp-content/themes/lacadev/theme/page_templates/template-portfolio.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Portfolio Page
+ *
+ * App Layout: layouts/app.php
+ *
+ * This is the template that is used for displaying portfolio page.
+ *
+ * @package WPEmergeTheme
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+$site_key = carbon_get_theme_option('recaptcha_site_key');
+?>
+
+<main id="main-content" class="portfolio-page-template">
+
+</main>
